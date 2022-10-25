45e1591cdce24f4029f84a7ad9c68b4cebafd855
DanCodes <dan@dancodes.online>
2022-10-25 10:23:30 +0100

    chore(deps): bump all

---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import path from 'path';
+
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+import replace from '@rollup/plugin-replace';
+import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension';
+import { emptyDir } from 'rollup-plugin-empty-dir';
+import zip from 'rollup-plugin-zip';
+
+const isProduction = process.env.NODE_ENV === 'production';
+export default {
+  input: 'src/manifest.json',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+    chunkFileNames: path.join('chunks', '[name]-[hash].js')
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': isProduction ? JSON.stringify('production') : JSON.stringify('development'),
+      preventAssignment: true
+    }),
+    chromeExtension(),
+    simpleReloader(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    emptyDir(),
+    isProduction && zip({ dir: 'releases' })
+  ]
+};
